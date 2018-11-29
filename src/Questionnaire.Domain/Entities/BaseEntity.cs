60b59d405244f0c0ac25ad3aeefac35ff2e5a3ba
Muhammad Ihsan <mihsan191@gmail.com>
2018-11-29 16:41:56 +1100

    Add domain project

---
+++ src/Questionnaire.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Questionnaire.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public string Id { get; }
+
+        public DateTimeOffset CreatedDate { get; }
+
+        public DateTimeOffset UpdatedDate { get; }
+    }
+}
---
+++ src/Questionnaire.Domain/Entities/Form.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Questionnaire.Domain.Entities
+{
+    public class Form : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
